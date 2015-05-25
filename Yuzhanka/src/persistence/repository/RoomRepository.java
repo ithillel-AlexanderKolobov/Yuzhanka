cdb299946174af28da38543bebe86d6d835f887e
Valentina <raton13@rambler.ru>
2015-05-25 23:50:49 +0300

    added addRoom, changeRoom, deleteRoom in RoomRepository

--- Yuzhanka/src/persistence/repository/RoomRepository.java
+++ Yuzhanka/src/persistence/repository/RoomRepository.java
@@ -1,6 +1,11 @@
 package persistence.repository;
 
+import org.hillel.it.yuzhanka.model.entity.Room;
+
 public interface RoomRepository
 {
-
+	boolean addRoom(Room room);
+	boolean changeRoom(Room newRoom);
+	boolean deleteRoom(Room room);
+	
 }

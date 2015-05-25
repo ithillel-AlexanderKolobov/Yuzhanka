6053da9e162afc890cae699a485d4f931a3d949c
Valentina <raton13@rambler.ru>
2015-05-26 00:24:41 +0300

    Added addRoomType, changeRoomType, deleteRoomType into RoomTypeRepository

---
+++ Yuzhanka/src/persistence/repository/RoomTypeRepository.java
@@ -0,0 +1,12 @@
+package persistence.repository;
+
+import org.hillel.it.yuzhanka.model.entity.RoomType;
+import org.hillel.it.yuzhanka.model.entity.User;
+
+public interface RoomTypeRepository
+{
+	boolean addRoomType(RoomType roomType);
+	boolean changeRoomType(RoomType newRoomType);
+	boolean deleteRoomType(RoomType roomType);
+
+}

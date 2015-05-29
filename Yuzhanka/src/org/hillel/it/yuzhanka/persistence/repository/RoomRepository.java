5576f3229185caaf71117099aef0a8ebfecf3bb6
Valentina <raton13@rambler.ru>
2015-05-29 11:32:45 +0300

    added attribute Room oldRoom into boolean changeRoom

--- Yuzhanka/src/org/hillel/it/yuzhanka/persistence/repository/RoomRepository.java
+++ Yuzhanka/src/org/hillel/it/yuzhanka/persistence/repository/RoomRepository.java
@@ -7,7 +7,7 @@ import org.hillel.it.yuzhanka.model.entity.Room;
 public interface RoomRepository
 {
 	boolean addRoom(Room room);
-	boolean changeRoom(Room newRoom);
+	boolean changeRoom(Room oldRoom, Room newRoom);
 	boolean deleteRoom(Room room);
 	Room getRoomById(int id);
 	List <Room> getAllRooms();

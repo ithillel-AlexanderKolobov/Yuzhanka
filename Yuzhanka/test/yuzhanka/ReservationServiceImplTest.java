b85f4b88bafbc52e319d8e9f94d5fecaa95d103a
Yurii Solopov <yuriysolopov@gmail.com>
2015-07-04 12:06:23 +0300

    resolve conflict

--- Yuzhanka/test/yuzhanka/ReservationServiceImplTest.java
+++ Yuzhanka/test/yuzhanka/ReservationServiceImplTest.java
@@ -3,21 +3,6 @@ package yuzhanka;
 
 import org.hillel.it.yuzhanka.application.service.ReservationService;
 import org.hillel.it.yuzhanka.application.service.impl.ReservationServiceImpl;
-import org.junit.Before;
-
-public class ReservationServiceImplTest {
-private ReservationService service;
-
-@Before
-public void setup() {
-	service = new ReservationServiceImpl();
-}
-}
-=======
-package yuzhanka;
-
-import org.hillel.it.yuzhanka.application.service.ReservationService;
-import org.hillel.it.yuzhanka.application.service.impl.ReservationServiceImpl;
 import org.hillel.it.yuzhanka.model.entity.Reservation;
 import org.junit.Before;
 import org.junit.Test;

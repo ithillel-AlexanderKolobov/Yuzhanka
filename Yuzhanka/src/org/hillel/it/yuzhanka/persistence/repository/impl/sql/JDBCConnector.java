3b9f7480f243fea2b8811eea7b066f621ee3fff8
Yurii Solopov <yuriysolopov@gmail.com>
2015-09-16 17:57:17 +0300

    SQL TEST

--- Yuzhanka/src/org/hillel/it/yuzhanka/persistence/repository/impl/sql/JDBCConnector.java
+++ Yuzhanka/src/org/hillel/it/yuzhanka/persistence/repository/impl/sql/JDBCConnector.java
@@ -1,10 +1,29 @@
 package org.hillel.it.yuzhanka.persistence.repository.impl.sql;
 
 import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import com.mysql.fabric.jdbc.FabricMySQLDriver;
 
 public class JDBCConnector {
-	private static Connection getDBConnection() {
-	    
-	    return null;
+
+	public static void main(String[] args) {
+
+		try {
+			Driver driver = new FabricMySQLDriver();
+			DriverManager.registerDriver(driver);
+
+		} catch (SQLException e) {
+			System.out.println("Не удалост стартануть драйвер");
+		}
+		try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/yuzjhanka", "root",
+				"root"); Statement statement = connection.createStatement()) {
+			statement.execute("INSERT INTO test_table VALUES(NULL,'Воронова');");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
